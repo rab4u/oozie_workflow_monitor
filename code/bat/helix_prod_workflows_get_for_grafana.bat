3f1158e50b18287d84ac63f81b7233b7e0b27d55
rab4u <rab4u@engineer.com>
2019-01-05 15:42:00 +0400

    Rename code/bat/helix_prod_workflows_get_for_grafana.bat to workflows_get_for_graf

--- code/bat/helix_prod_workflows_get_for_grafana.bat
+++
@@ -1,1 +0,0 @@
-curl "http://<oozie_server_address>:11000/oozie/v2/jobs?len=110000" -o <path>\data\json\oozie_jobs_trends.json
---
+++ workflows_get_for_graf
@@ -0,0 +1,1 @@
+curl "http://<oozie_server_address>:11000/oozie/v2/jobs?len=110000" -o <path>\data\json\oozie_jobs_trends.json
